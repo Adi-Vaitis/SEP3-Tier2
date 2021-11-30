65cd6cba5aafb57fcaa1edc6e1acbc1fa71deb94
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-11-30 12:28:29 +0100

    fixing stuff

--- src/main/java/tier2/services/client/IClientService.java
+++ src/main/java/tier2/services/client/IClientService.java
@@ -5,4 +5,14 @@ import tier2.models.Client;
 public interface IClientService
 {
   Client validateClient(String Username, String Password);
+
+  String createClientAccount(Client client);
+
+  String editAccount(Client client);
+
+  void deleteClient(int clientId);
+
+  Client getClientByUsername(String username);
+
+  Client getClientById(int clientId);
 }
