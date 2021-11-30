662145e78d50e16f3831879d0cd50c770aadcdbd
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-11-30 11:32:26 +0100

    Something just messed everything up, so decided to have the tier 2 in a different repository

---
+++ src/test/java/tier2/tier2/Tier2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tier2.tier2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tier2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
